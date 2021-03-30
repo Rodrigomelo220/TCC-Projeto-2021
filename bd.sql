6ba7a9c3ca60236b8aab3ccfb46c313ea4fe9578
Rodrigo Melo <rodrigomelo220@gmail.com>
2021-03-29 23:57:10 -0300

    Create bd.sql

---
+++ bd.sql
@@ -0,0 +1,9 @@
+create database psoftware;
+use psoftware;
+
+create table usuario (
+    usuario_id int not null primary key auto_increment,
+    usuario_nome VARCHAR(200) not null,
+    usuario_email VARCHAR(200) not null,
+    usuario_senha VARCHAR(200) not null
+)
